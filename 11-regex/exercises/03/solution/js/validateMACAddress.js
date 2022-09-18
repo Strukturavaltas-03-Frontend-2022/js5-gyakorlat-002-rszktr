daa7c2d4e1434ee58016c264071e82854a4af843
Joe <reszketo.robert@gmail.com>
2022-09-18 14:58:43 +0200

    solution

--- 11-regex/exercises/03/solution/js/validateMACAddress.js
+++ 11-regex/exercises/03/solution/js/validateMACAddress.js
@@ -1,1 +1,8 @@
+const validateMACAddress = (input) => {
+  // XX:XX:XX:XX:XX:XX, but will sometimes display as XX-XX-XX-XX-XX-XX or XXXXXXXXXXXX
+  const pattern = /^((\d|[A-Fa-f]){2}[:-]){5}(\d|[A-Fa-f]){2}$/;
+
+  return !!(input.match(pattern));
+};
+
 export default validateMACAddress;
--- 11-regex/exercises/04/solution/js/validate.js
+++ 11-regex/exercises/04/solution/js/validate.js
@@ -1,1 +1,12 @@
+const roles = {
+  visa: /^4(\d{12}|\d{15})$/,
+  ip: /^(25[0-5]|2[0-4][0-9]|1\d\d|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|1\d\d|[1-9][0-9]|[0-9]\.){2}(25[0-5]|2[0-4][0-9]|1\d\d|[1-9][0-9]|[1-9])$/,
+  mac: /^((\d|[A-Fa-f]){2}[:-]){5}(\d|[A-Fa-f]){2}$/,
+  validate(text, type) {
+    return !!text.match(this.patterns[type]);
+  },
+};
+
+const validate = (string, format) => !!string.match(roles[format]);
+
 export default validate;
